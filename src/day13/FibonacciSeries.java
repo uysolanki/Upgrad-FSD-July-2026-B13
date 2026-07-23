9c34ced2dbd488407d6f24017aad0585297f9d56
uysolanki <uysolanki@gmail.com>
2026-07-23 13:17:07 +0530

    git documentation

---
+++ src/day13/FibonacciSeries.java
@@ -0,0 +1,26 @@
+package day13;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+public static void main(String[] args) {
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Length of Fibonacci Series");
+	int length=sc.nextInt();
+	int n1=0;
+	int n2=1;
+	
+	System.out.println(n1);
+	System.out.println(n2);
+	
+	for(int i=3;i<=length;i++)
+	{
+		int n3=n1+n2;
+		System.out.println(n3);
+		n1=n2;
+		n2=n3;
+	}
+	
+}
+}
---
+++ src/day13/SumOfDigit3.java
@@ -0,0 +1,26 @@
+package day13;
+
+import java.util.Scanner;
+
+public class SumOfDigit3 {
+public static void main(String[] args) {
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a Number");  //777
+	int n=sc.nextInt();						
+	int sum=0;
+	while(n>9)									//n							sum					rem
+	{											//777		is 777>9 true	0	is 777>0 true   7
+			sum=0;							//77				        7   is 77>0 true    7
+			while(n>0)							//7                         14	is 7>0 true     7
+			{									//0                         21	is 0>0 false
+				int rem=n%10;					//21        is 21>9  true   0   is 21>0 true
+				sum=sum+rem;   					//2                         1   is 2>0 true     1
+				n=n/10;							//0                         3   is 0>0 fasle    2
+			}									//3         is 3>9 false   output 3
+			n=sum;
+	}
+	
+	System.out.println(sum);
+}
+}
