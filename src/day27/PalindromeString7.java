1c7ad5e3f5c15ac9d0015bf81111982de12fe47b
uysolanki <uysolanki@gmail.com>
2026-08-11 14:01:41 +0530

    palindrome string removing specia chars

---
+++ src/day27/PalindromeString7.java
@@ -0,0 +1,32 @@
+//removing special chars
+package day27;
+
+public class PalindromeString7 {
+public static void main(String[] args) {
+	                                
+	                               
+	//String str="nitin";				
+	//String str="nitina";	
+	String str="a * nut for : a jar of  _ tuna";
+	System.out.println(str);
+    str=str.replaceAll("[^A-Za-z]","");
+	System.out.println(str);
+	//System.out.println(checkPalindrome(str)?"Palindrome":"Not Palindrome");
+	
+}
+
+private static boolean checkPalindrome(String str) {
+	int first=0;
+	int last=str.length()-1;
+	while(first<last)
+	{
+		if(str.charAt(first)!=str.charAt(last))
+		{
+			return false;
+		}
+		first++;
+		last--;
+	}
+	return true;
+}
+}
