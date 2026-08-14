2ba7affb89235187c864c0c82ead2ab7876bd1b4
uysolanki <uysolanki@gmail.com>
2026-08-14 13:12:47 +0530

    call by value using object

---
+++ src/day30/Test.java
@@ -0,0 +1,18 @@
+package day30;
+
+class Test {
+
+	private int x;
+
+	public void display() {
+		System.out.println(x);
+	}
+
+	public void setX(int n) {
+		this.x = n;
+	}
+
+	public int getX() {
+		return this.x;
+	}
+}
---
+++ src/day30/TestDriverApp.java
@@ -0,0 +1,23 @@
+package day30;
+
+public class TestDriverApp {
+
+	public static void main(String[] args) {
+	Test t1=new Test();
+	t1.display();				//0
+	t1.setX(99);
+	t1.display();				//99
+	
+	upgrad(t1);
+	t1.display();				//333
+	}
+
+	private static void upgrad(Test t2) {
+		// TODO Auto-generated method stub
+		t2.setX(333);
+		
+		t2=new Test();
+		t2.setX(777);
+	}
+
+}
