9576178475237f483a226dbd654b7110c26f662a
uysolanki <uysolanki@gmail.com>
2026-08-18 16:24:48 +0530

    validation & exception handling

---
+++ src/day32/ReferemceMethodDemo.java
@@ -0,0 +1,21 @@
+package day32;
+
+public class ReferemceMethodDemo {
+
+	public static void main(String[] args) {
+		//Student s1=new Student();
+		
+		//ref    = object
+		
+		Student s1;
+		
+		s1=new Student();
+		
+		System.out.println(s1);
+		
+		
+		
+
+	}
+
+}
---
+++ src/day32/Student.java
@@ -0,0 +1,86 @@
+package day32;
+
+public class Student {
+	
+	private int rno;						
+	private String studentName;				
+	private double per;						
+	
+	
+	public Student()   //NoArgsConstructor
+	{
+		this.rno=1;
+		this.studentName="Rahul";
+		this.per=40.0;
+	}
+	
+	public Student(int x, String y, double z)   //AllArgsConstructor
+	{
+		this.rno=x;
+		this.studentName=y;
+		this.per=z;
+	}
+	
+	public Student(Student st)   //CopyConstructor
+	{
+		this.rno=st.rno;
+		this.studentName=st.studentName;
+		this.per=st.per;
+	}
+	
+	public void displayStudent() 
+	{ 
+		System.out.println("Roll Number is "+this.rno);
+		System.out.println("Student Name is "+this.studentName);
+		System.out.println("Percenatage is "+this.per);
+	}
+
+	public int getRno() {
+		return rno;
+	}
+
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public double getPer() {
+		return per;
+	}
+
+	public void setPer(double per) {
+		this.per = per;
+	}
+	
+	public boolean searchByName(String name)
+	{
+		return this.studentName.equals(name);
+	}
+
+	public boolean searchByRollNumber(int searchedRno) {
+		if(this.rno==searchedRno)
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rno=" + rno + ", studentName=" + studentName + ", per=" + per + "]";
+	}
+	
+	
+//	@Override
+//	public String toString() {
+//		return "My name is Shoyab";
+//	}
+	
+	
+}
