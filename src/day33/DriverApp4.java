8182ca22e22fe724fd8e10c780b757b2771d55f3
uysolanki <uysolanki@gmail.com>
2026-08-19 14:50:32 +0530

    upcasting in method call and return

---
+++ src/day33/DriverApp4.java
@@ -0,0 +1,41 @@
+package day33;
+
+public class DriverApp4 {
+
+	public static void main(String[] args) {
+		int n=5;
+		char m='A';
+		double l=10.8;
+		test(new Abhishek());
+		
+		demo();
+		
+		
+	}
+
+	public static void test(Amitabh t)
+	{
+		
+	}
+	
+	public static int demo()
+	{
+		
+		return 1;
+	}
+	
+	public static char demo1()
+	{
+		
+		return 'A';
+	}
+	
+	public static Amitabh demo2()
+	{
+		
+//		return new Amitabh();
+//		return new Abhishek();
+		return new Aradhya();
+	}
+
+}
