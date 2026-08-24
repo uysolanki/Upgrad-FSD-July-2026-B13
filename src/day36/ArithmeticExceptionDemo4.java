849758cd120b01cfb9e44ab0b475a50c66b91a9f
uysolanki <uysolanki@gmail.com>
2026-08-24 14:26:06 +0530

    multiple catch

--- src/day36/ArithmeticExceptionDemo4.java
+++ src/day36/ArithmeticExceptionDemo4.java
@@ -3,7 +3,7 @@ package day36;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class ArithmeticExceptionDemo2 {
+public class ArithmeticExceptionDemo4 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -13,25 +13,16 @@ public class ArithmeticExceptionDemo2 {
 		int numerator = sc.nextInt();
 		System.out.println("Enter Denominator"); 
 		int denominator = sc.nextInt();
-		//String name="Alice";
-		String name=null;
-		System.out.println(name.length());
+		
 		double result=numerator/denominator;   //new ArithmeticException()
 		System.out.println(result);
 		}
-		catch(ArithmeticException ex)
+		catch(ArithmeticException | InputMismatchException ex)
 		{
-//			System.out.println(ex.getMessage());
-			System.out.println("Denominator should be non zero value");
-		}
-		catch(InputMismatchException ex)
-		{
-//			System.out.println(ex.getMessage());
-			System.out.println("Please enter valid integer values only");
+			System.out.println("Invalid Input");
 		}
 		catch(Exception ex)
 		{
-//			System.out.println(ex.getMessage());
 			System.out.println("some exception occured");
 		}
 		
