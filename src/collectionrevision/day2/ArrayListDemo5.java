770967d6bba6b0dbe59dcedb6be2382da9f43089
uysolanki <uysolanki@gmail.com>
2026-09-22 13:33:20 +0530

    collection revision day 2

---
+++ src/collectionrevision/day2/ArrayListDemo5.java
@@ -0,0 +1,212 @@
+package collectionrevision.day2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListDemo5 {
+
+	public static void main(String[] args) {
+	
+		Player rohit = new Player(
+                45,
+                "Rohit Sharma",
+                250,
+                11000,
+                100,
+                0,
+                1000,
+                300,
+                30,
+                "Batsman",
+                List.of("World Cup", "Champions Trophy", "IPL")
+        );
+
+        Player surya = new Player(
+                63,
+                "Suryakumar Yadav",
+                150,
+                5000,
+                80,
+                0,
+                500,
+                250,
+                5,
+                "Batsman",
+                List.of("T20 World Cup", "IPL")
+        );
+
+        Player pandya = new Player(
+                33,
+                "Hardik Pandya",
+                180,
+                4500,
+                90,
+                150,
+                350,
+                200,
+                0,
+                "All Rounder",
+                List.of()
+        );
+
+        Player bumrah = new Player(
+                93,
+                "Jasprit Bumrah",
+                180,
+                500,
+                70,
+                350,
+                30,
+                5,
+                0,
+                "Bowler",
+                List.of("World Cup", "Champions Trophy")
+        );
+
+        Player boult = new Player(
+                18,
+                "Trent Boult",
+                150,
+                400,
+                60,
+                300,
+                20,
+                2,
+                0,
+                "Bowler",
+                List.of("World Cup", "IPL")
+        );
+
+        Player williamson = new Player(
+                22,
+                "Kane Williamson",
+                200,
+                9000,
+                100,
+                0,
+                800,
+                100,
+                25,
+                "Batsman",
+                List.of("World Test Championship")
+        );
+
+        Player ghazanfar = new Player(
+                21,
+                "Allah Ghazanfar",
+                20,
+                100,
+                5,
+                25,
+                5,
+                2,
+                0,
+                "Bowler",
+                List.of()
+        );
+
+        Player nabi = new Player(
+                7,
+                "Mohammad Nabi",
+                200,
+                3500,
+                100,
+                200,
+                300,
+                100,
+                0,
+                "All Rounder",
+                List.of("Asia Cup", "IPL")
+        );
+
+        Player quinton = new Player(
+                12,
+                "Quinton de Kock",
+                180,
+                8000,
+                150,
+                0,
+                750,
+                150,
+                25,
+                "Wicket Keeper Batsman",
+                List.of("World Cup", "IPL")
+        );
+
+        Player steyn = new Player(
+                8,
+                "Dale Steyn",
+                100,
+                400,
+                50,
+                400,
+                30,
+                2,
+                0,
+                "Bowler",
+                List.of("IPL")
+        );
+        
+        Player sachin = new Player(
+                8,
+                "Sachin Tendulkar",
+                200,
+                800,
+                100,
+                800,
+                30,
+                2,
+                0,
+                "Batsmen",
+                List.of("IPL","World Cup", "Champions Trophy")
+        );
+        
+        
+        List<Player> IndianPlayers=new ArrayList();
+        IndianPlayers.add(rohit);
+        IndianPlayers.add(surya);
+        IndianPlayers.add(bumrah);
+        IndianPlayers.add(pandya);
+        IndianPlayers.add(sachin);
+        
+        
+        List<Player> newZealandPlayers=new ArrayList();
+        newZealandPlayers.add(boult);
+        newZealandPlayers.add(williamson);
+        
+        List<Player> southAfricaPlayers=new ArrayList();
+        southAfricaPlayers.add(quinton);
+        southAfricaPlayers.add(steyn);
+		
+        List<Player> afghanistanPlayers=new ArrayList();
+        afghanistanPlayers.add(ghazanfar);
+        afghanistanPlayers.add(nabi);
+        
+        List<Player> mumbaiIndians=new ArrayList();
+        mumbaiIndians.addAll(IndianPlayers);
+        mumbaiIndians.addAll(newZealandPlayers);
+        mumbaiIndians.addAll(southAfricaPlayers);
+        mumbaiIndians.addAll(afghanistanPlayers);
+        
+        System.out.println(mumbaiIndians.size());
+        
+        //display average runs of mi team
+        int totalRuns=0;
+        for(Player player:mumbaiIndians)
+        {
+        	totalRuns+=player.getRunsScored();
+        }
+        System.out.println("Average runs of Mumbai Indians" +totalRuns/mumbaiIndians.size());
+        
+        
+      //display name of all mi players
+        System.out.println("*** MI Squad ****");
+        for(Player player:mumbaiIndians)
+        {
+        	System.out.println(player.getPlayerName() + "(" + player.getCategory() + ")");
+        }
+        
+	}
+
+}
---
+++ src/collectionrevision/day2/Player.java
@@ -0,0 +1,148 @@
+package collectionrevision.day2;
+
+import java.util.List;
+
+public class Player {
+
+    private int jerseyNumber;
+    private String playerName;
+    private int matchesPlayed;
+    private int runsScored;
+    private int catchesTaken;
+    private int wicketsTaken;
+    private int fours;
+    private int sixes;
+    private int centuries;
+    private String category;
+    private List<String> trophies;
+
+    // Default Constructor
+    public Player() {
+    }
+
+    // Parameterized Constructor
+    public Player(int jerseyNumber, String playerName, int matchesPlayed,
+                  int runsScored, int catchesTaken, int wicketsTaken,
+                  int fours, int sixes, int centuries,
+                  String category, List<String> trophies) {
+
+        this.jerseyNumber = jerseyNumber;
+        this.playerName = playerName;
+        this.matchesPlayed = matchesPlayed;
+        this.runsScored = runsScored;
+        this.catchesTaken = catchesTaken;
+        this.wicketsTaken = wicketsTaken;
+        this.fours = fours;
+        this.sixes = sixes;
+        this.centuries = centuries;
+        this.category = category;
+        this.trophies = trophies;
+    }
+
+    // Getters and Setters
+
+    public int getJerseyNumber() {
+        return jerseyNumber;
+    }
+
+    public void setJerseyNumber(int jerseyNumber) {
+        this.jerseyNumber = jerseyNumber;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getMatchesPlayed() {
+        return matchesPlayed;
+    }
+
+    public void setMatchesPlayed(int matchesPlayed) {
+        this.matchesPlayed = matchesPlayed;
+    }
+
+    public int getRunsScored() {
+        return runsScored;
+    }
+
+    public void setRunsScored(int runsScored) {
+        this.runsScored = runsScored;
+    }
+
+    public int getCatchesTaken() {
+        return catchesTaken;
+    }
+
+    public void setCatchesTaken(int catchesTaken) {
+        this.catchesTaken = catchesTaken;
+    }
+
+    public int getWicketsTaken() {
+        return wicketsTaken;
+    }
+
+    public void setWicketsTaken(int wicketsTaken) {
+        this.wicketsTaken = wicketsTaken;
+    }
+
+    public int getFours() {
+        return fours;
+    }
+
+    public void setFours(int fours) {
+        this.fours = fours;
+    }
+
+    public int getSixes() {
+        return sixes;
+    }
+
+    public void setSixes(int sixes) {
+        this.sixes = sixes;
+    }
+
+    public int getCenturies() {
+        return centuries;
+    }
+
+    public void setCenturies(int centuries) {
+        this.centuries = centuries;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public List<String> getTrophies() {
+        return trophies;
+    }
+
+    public void setTrophies(List<String> trophies) {
+        this.trophies = trophies;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "jerseyNumber=" + jerseyNumber +
+                ", playerName='" + playerName + '\'' +
+                ", matchesPlayed=" + matchesPlayed +
+                ", runsScored=" + runsScored +
+                ", catchesTaken=" + catchesTaken +
+                ", wicketsTaken=" + wicketsTaken +
+                ", fours=" + fours +
+                ", sixes=" + sixes +
+                ", centuries=" + centuries +
+                ", category='" + category + '\'' +
+                ", trophies=" + trophies +
+                '}';
+    }
+}
